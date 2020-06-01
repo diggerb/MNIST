b09de5384a9694c4347d935054818622b033e392
diggerb <james@briant.co.uk>
2020-06-01 19:26:47 +0100

    calls open_firefox_tab.bat

--- start_jupyter.bat
+++ start_jupyter.bat
@@ -1,12 +1,12 @@
-:: This program pulls from git, then opens git bash in the working directory starts the jupyter notebook.
-:: Once the jupyter notebook has closed one final commit and push is completed.
+:: This program pulls from git, then opens git bash in the working directory starts the jupyter notebook and runs opne_firefox_tab.bat.
+:: Once the jupyter container has stopped one final commit and push is completed.
+
 :: Commits should be done regularly while working including just before quitting the notebook.
 
-git pull origin master
-start "" "%PROGRAMFILES%\Git\bin\sh.exe" --login
-start firefox -new-tab localhost:8888
+::git pull origin master
+::start "" "%PROGRAMFILES%\Git\bin\sh.exe" --login
+start cmd /c call "./open_firefox_tab.bat"
 docker run -it --rm -p 8888:8888 -v %cd%:/tf/notebooks tensorflow-pandas
-docker-compose -f docker-compose.jupyter.yml up
-git add .
-git commit -m "Closing notebook"
-git push origin master
+::git add .
+::git commit -m "Closing notebook"
+::git push origin master
