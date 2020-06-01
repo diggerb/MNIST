ced62e4356e0883d9f0bce78be3140a9d2e71c8f
diggerb <james@briant.co.uk>
2020-06-01 17:07:42 +0100

    Closing notebook

--- start_jupyter.bat
+++ start_jupyter.bat
@@ -2,11 +2,11 @@
 :: Once the jupyter notebook has closed one final commit and push is completed.
 :: Commits should be done regularly while working including just before quitting the notebook.
 
-::git pull origin master
-::start "" "%PROGRAMFILES%\Git\bin\sh.exe" --login
+git pull origin master
+start "" "%PROGRAMFILES%\Git\bin\sh.exe" --login
 start firefox -new-tab localhost:8888
 docker run -it --rm -p 8888:8888 -v %cd%:/tf/notebooks tensorflow-pandas
-::docker-compose -f docker-compose.jupyter.yml up
-::git add .
-::git commit -m "Closing notebook"
-::git push origin master
+docker-compose -f docker-compose.jupyter.yml up
+git add .
+git commit -m "Closing notebook"
+git push origin master
