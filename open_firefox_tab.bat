99b43835e1322edbe3a64e47154086b7abd77b83
diggerb <james@briant.co.uk>
2020-06-01 19:37:20 +0100

    changed timeout from 3.5 to 3 due to error

--- open_firefox_tab.bat
+++ open_firefox_tab.bat
@@ -1,7 +1,7 @@
 :: Finds the running docker container ID, retrieves the token from within the container and opens a new firefox tab
 
 REM timeout allows container to start before opening the new tab
-timeout /t 3.5 /nobreak
+timeout /t 3 /nobreak
 
 for /f "delims=" %%i in ('docker ps -aqf "ancestor=tensorflow-pandas"') do set ID=%%i
 for /f "delims=" %%i in ('docker exec %ID% jupyter notebook list') do set URL=%%i
@@ -9,3 +9,4 @@ for /f "delims=" %%i in ('docker exec %ID% jupyter notebook list') do set URL=%%
 set URL=%URL: :: /tf=%
 set URL=%URL:0.0.0.0=127.0.0.1%
 start firefox -new-tab %URL%
+PAUSE
